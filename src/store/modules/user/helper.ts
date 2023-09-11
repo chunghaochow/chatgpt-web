d7c08a4ccc4bd1f3b9f95425b010946028b3c6e8
hao <agamesfan@gmail.com>
2023-09-11 17:18:36 +0800

    update avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-			avatar: 'https://i.328888.xyz/2023/04/15/i7H6np.th.png',
+			avatar: '',
 			name: 'Mario',
 			description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
